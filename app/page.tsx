e8a428c58be2b9433285c49500fe4aba0fd90189
Klaas Pieter Annema <kp@annema.me>
2024-01-16 13:23:14 +0100

    Use markdown for main text

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,26 @@
+import { kMaxLength } from "buffer";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
+import path from "path";
+import { promises as fs } from "fs";
+import { remark } from "remark";
+import html from "remark-html";
 
-export default function Home() {
+export default async function Home() {
+  const mdPath = path.join(process.cwd(), "app", "text.md");
+  const markdown = await fs.readFile(mdPath, "utf8");
+  const processedContent = await remark().use(html).process(markdown);
+  const contentHtml = processedContent.toString();
+
   return (
     <main className="flex min-h-screen flex-col items-leading p-12 prose dark:prose-invert">
       <h1>Het Eemgoed</h1>
       <h2>Dorpslandgoed van leven in verbinding</h2>
-      <p>
-        Aan de rand van Almere Oosterwold, naast de Groene Kathedraal en het
-        voedselbos van Staatsbosbeheer ligt dorpslandgoed het Eemgoed. Een
-        gemeenschap van tweeëntachtig huishoudens die leven in verbinding met de
-        natuur en met elkaar.
-      </p>
-      <ul>
-        <li>82 woningen (plaatje) </li>
-        <li>10 landschapskamers</li>
-        <li>1 gemeenschapshuis - het Eemhuis</li>
-      </ul>
-      <h3>Wonen op het Eemgoed</h3>
-      <p>
-        Er zijn verschillende redenen om op het Eemgoed te komen wonen. Voor
-        veel bewoners speelt sociale verbinding een belangrijke rol. Nog voordat
-        de eerste heipalen van het Eemgoed de grond in gingen, zochten de
-        toekomstige bewoners elkaar al op. Tijdens gezellige koffieochtenden
-        deelden ze hun dromen en plannen met elkaar.
-      </p>
-      <p>
-        In 2022 betrokken de meeste Eemgoeders hun woning en begonnen ze hun
-        plannen waar te maken. Bewoners onderhouden samen de moestuinen, de
-        boomgaarden en het stilte-eiland. Ze zoeken elkaar op in het
-        gemeenschapshuis[h][i][j] om te werken en voor de gezelligheid. Ze
-        organiseren er bijeenkomsten, workshops en feesten, of zijn druk in de
-        weer in de werkplaats of de landschapsschuur.
-      </p>
-      <p>
-        Goede tijden worden gevierd. In moeilijke tijden staan we voor elkaar
-        klaar. We helpen elkaar met kinderen van school halen, samen eten maken
-        met de groenten uit de tuin, of het fixen van een fiets of de tractor.
-        Zelfs deze website hebben we samen gemaakt. Iedereen doet wat hij of zij
-        kan. En dat is genoeg.
-      </p>
-      <h3>Ecologisch Eemgoed en Cepezed</h3>
-      <p>
-        De prachtige woningen van het Eemgoed zijn ontworpen door gerenommeerd
-        architectenbureau Cepezed. De hoge, karakteristieke glazen puien maken
-        de woningen het hele jaar door licht en behaaglijk. Door het slimme
-        ontwerp gaan de zonnepanelen die aan de tuinkant boven de ramen ‘zweven’
-        op in het design. Ecologie is een belangrijk aspect in het ontwerp van
-        het Eemgoed. De woningen zijn voorzien van warmtepompen en voldoende
-        zonnepanelen om bijna het hele jaar door energieneutraal te zijn.
-        Gezamenlijk wordt er gewerkt aan het Eemgoed als energie community van
-        de toekomst. Een zonnedak op het Eemhuis zorgt voor duurzame lokale
-        opwekking van zonnestroom om te gebruiken in de laadpalen van het
-        gezamenlijke laadplein. Op basis van het concept Local for Local werken
-        we mee aan de democratisering van de energiemarkt waar we zoveel
-        mogelijk onze eigen energie opwekken, opslaan en verbruiken. De
-        voorgevel van de huizen zal bestaan uit een groen ‘talud’, een met gras
-        ingezaaide heuvel. Deze is ten tijde van schrijven (4e kwartaal 2023)
-        nog in aanbouw. De paden langs en tussen de huizen zijn autoluw zodat er
-        genoeg rust en ruimte is om over het Eemgoed te lopen en om te spelen.
-      </p>
-      <h3>Toegang tot het Eemgoed</h3>
-      <p>
-        De 7 hectare van het Eemgoed zijn privé terrein van de bewoners. De
-        parkeerplaats, paden, landschapskamers, speeltuin en het Eemhuis zijn
-        enkel toegankelijk voor bewoners en hun gasten. In lijn met de regels
-        van Almere Oosterwold loopt rondom het Eemgoed een ‘doorwaadbare zone’
-        die toegankelijk is voor iedereen. Deze loopt vanaf de Oeverwalwegrechts
-        langs het gemeenschapshuis voorlangs de eerste rij huizen, langs de
-        kavelsloot en de twee bruggen die toegang bieden tot het voedselbos,
-        achter de landschapskamer van de laatste rij huizen weer terug naar de
-        Oeverwalweg.
-      </p>
 
-      <h3>Het Eemgoed bezoeken</h3>
-      <p>
-        Er is veel interesse in de manier van wonen en samenleven op ons
-        dorpslandgoed. We zijn trots op onze samenleving, ons dorp in het klein
-        en leiden je graag rond. Je kunt een afspraak maken voor een rondleiding
-        over het Eemgoed. Je krijgt dan uitleg over de architectuur van de
-        huizen en het terrein, de geschiedenis van Oosterwold en over onze
-        invulling van het in Oosterwold welbekende ‘stadslandbouw’. Een
-        rondleiding begint bij Voedselbos Eemvallei en gaat te voet naar het
-        Eemgoed. De rondleiding eindigt in het gemeenschappelijk huis. Iedereen
-        is welkom om op afspraak te komen kijken. Zowel kleine gezelschappen,
-        als grotere georganiseerde groepen kunnen contact opnemen met … via … om
-        een rondleiding te boeken.
-      </p>
-
-      <p>
-        In verband met de privacy en het woongenot van bewoners verzoeken we
-        alle geïnteresseerde bezoekers een afspraak te maken als ze naar Eemgoed
-        willen komen. Ter zijner tijd wordt deze website uitgebreid met meer
-        informatie over Eemgoed en zijn omgeving. Waarom tijdelijk Eemgoed?
-        Misschien is de url van onze website je opgevallen,
-        ‘tijdelijk-Eemgoed.nl’. Onze woongemeenschap is jong en nog volop in
-        beweging. We zijn onze identiteit nog aan het vormgeven. Daarom tot die
-        tijd: tijdelijk-Eemgoed. Contact Voor overige vragen of verzoeken kun je
-        per e-mail contact opnemen met het Eemgoed VVE Bestuur.
-      </p>
+      <article
+        className="prose dark:prose-invert"
+        dangerouslySetInnerHTML={{ __html: contentHtml }}
+      />
     </main>
   );
 }
