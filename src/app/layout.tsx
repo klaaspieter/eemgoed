c8e652e7f2b76b1e7a813d88b6a3c687cb9a34e1
Klaas Pieter Annema <kp@annema.me>
2024-08-26 16:59:40 +0200

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import Navigation from "./components/Navigation";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className}`}>
         <Navigation />
         <main>{children}</main>
+        <GoogleAnalytics gaId="G-ZC41313JZ3" />
       </body>
     </html>
   );
